d3b4df3c0b0d7c01d1ca1b339ad192819ad80963
Anastasiia Shatylo <a.shatilo18@gmail.com>
2026-03-13 18:30:38 +0100

    V7.1
    Change icons link

--- src/js/action-required.js
+++ src/js/action-required.js
@@ -12,8 +12,16 @@ export async function actionRequiredInit() {
                 (item, index) =>
                   `<li class="action-required-item">
                     <div class="action-required-item-info">
-                      <svg class="action-required-item-icon" width="32" height="32">
-                        <use href="${new URL('../img/svg/icons.svg', import.meta.url).href}#icon-settings" />
+                      <svg
+                        class="action-required-item-icon"
+                        width="32"
+                        height="32"
+                        viewBox="0 0 32 32"
+                        aria-hidden="true"
+                      >
+                        <path d="M15.999 8.335c0.552 0 1 0.448 1 1v8c0 0.552-0.448 1-1 1s-1-0.448-1-1v-8c0-0.552 0.448-1 1-1z"></path>
+                        <path d="M15.999 22.668c0.736 0 1.333-0.597 1.333-1.333s-0.597-1.333-1.333-1.333c-0.736 0-1.333 0.597-1.333 1.333s0.597 1.333 1.333 1.333z"></path>
+                        <path d="M1.666 16.001c0-7.916 6.417-14.333 14.333-14.333s14.333 6.417 14.333 14.333-6.417 14.333-14.333 14.333c-7.916 0-14.333-6.417-14.333-14.333zM15.999 3.668c-6.811 0-12.333 5.522-12.333 12.333s5.522 12.333 12.333 12.333c6.812 0 12.333-5.522 12.333-12.333s-5.522-12.333-12.333-12.333z"></path>
                       </svg>
                       <p class="action-required-name">${item.name}</p>
                     </div>
--- src/js/personal-info.js
+++ src/js/personal-info.js
@@ -14,8 +14,14 @@ export async function personalInfoInit() {
               <p class="personal-info-type">${item.name}</p>
               <div class="personal-info-right">
                 <p class="personal-info-value">${item.content ?? ''}</p>
-                <svg class="personal-info-icon" width="11" height="19">
-                  <use href="${new URL('../img/svg/icons.svg', import.meta.url).href}#icon-arrow" />
+                <svg
+                  class="personal-info-icon"
+                  width="11"
+                  height="19"
+                  viewBox="0 0 20 32"
+                  aria-hidden="true"
+                >
+                  <path d="M17.618 15.111c0 0.337-0.067 0.656-0.202 0.959-0.123 0.292-0.325 0.572-0.606 0.841l-12.94 12.671c-0.438 0.426-0.97 0.639-1.599 0.639-0.404 0-0.78-0.101-1.127-0.303s-0.628-0.471-0.841-0.808c-0.202-0.337-0.303-0.718-0.303-1.144 0-0.617 0.236-1.167 0.707-1.649l11.526-11.207-11.526-11.207c-0.471-0.471-0.707-1.021-0.707-1.649 0-0.415 0.101-0.791 0.303-1.127 0.213-0.348 0.494-0.623 0.841-0.825s0.724-0.303 1.127-0.303c0.628 0 1.161 0.213 1.599 0.639l12.94 12.671c0.269 0.269 0.471 0.55 0.606 0.841s0.202 0.611 0.202 0.959z"></path>
                 </svg>
               </div>
             </li>
